a86c5eeb6cea135eb98a966b496ee9c14a36137c
Raajmohan-rm1625 <rm1625@srmist.edu.in>
2026-02-17 22:28:11 +0530

    Add files via upload

--- OOPSbannerApp.java
+++ OOPSbannerApp.java
@@ -1,5 +1,12 @@
-public class OOPSbannerApp{
+public class OOPSBannerApp2{
 	public static void main(String[] args){
-	System.out.println("OOPS");
+	System.out.println("    ***		" + " " + "     ***		" + " " + "		*******	        " + " " + "    *****");
+	System.out.println(" **		**  " + " " + "	 **		**  " + " " + "     **     **       " + " " + "  **      ");
+	System.out.println("**		 ** " + " " + "**		 ** " + " " + "	    **      **      " + " " + " **       ");
+	System.out.println("**		 ** " + " " + "**		 ** " + " " + "     **       **	    " + " " + "   *****  ");
+	System.out.println("**		 ** " + " " + "**		 ** " + " " + "     ***********	    " + " " + "       **");
+	System.out.println(" **		**  " + " " + "	**		**  " + " " + "	    **              " + " " + " **     **");
+	System.out.println("    ***    " + " " + "     ***		" + " " + "	    **              " + " " + "	  ****   ");
 	}
 }
+ 
